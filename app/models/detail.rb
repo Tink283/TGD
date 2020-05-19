2907b0ad7932871043240f4a8a0c94527fbe970a
octpus <gengen712@outlook.jp>
2020-05-20 05:31:44 +0900

    画像投稿

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,2 +1,12 @@
 class Detail < ApplicationRecord
+    mount_uploader :image1, ImgUploader
+    mount_uploader :image2, ImgUploader
+    mount_uploader :image3, ImgUploader
+    mount_uploader :image4, ImgUploader
+    mount_uploader :image5, ImgUploader
+    mount_uploader :image6, ImgUploader
+    mount_uploader :image7, ImgUploader
+    mount_uploader :image8, ImgUploader
+    mount_uploader :image9, ImgUploader
+    mount_uploader :image10, ImgUploader
 end
