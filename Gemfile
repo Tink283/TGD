e96ea29384c2bc2f6ce393c083f03d80db393941
Tink283 <56713397+Tink283@users.noreply.github.com>
2020-06-04 01:37:32 +0900

    6/3

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'carrierwave', '~> 1.3', '>= 1.3.1'
 
 gem 'ransack'
+
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :details
   resources :plans
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   get "plans/new" => "plans#new"
   get "plans/show" => "plans#show"
   get "plans/edit" => "plans#edit"
